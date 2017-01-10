b12d148746a1d5f35c90551dc33869c97cf0d636
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-01-09 18:18:06 -0800

    Added total function to objects and called Functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var hours = ['6am','7am','8am','9am','10am', '11am', '12pm', '1pm','2pm', '3pm', '4pm', '5pm', '6pm', '7pm','8pm'];
 
+// First Store 1st and Pike
 var firstNpike = {
   minCustPerHour: 23,
   maxCustPerHour: 65,
@@ -19,9 +20,19 @@ var firstNpike = {
       console.log('Number of cookies purchased per hour is ' + this.cookiesPurchased[i]);
     }
     return this.cookiesPurchased;
+  },
+  totalCookies: function () {
+    var sum = 0;
+    for (var i = 0; i < hours.length ; i++) {
+      sum += this.cookiesPurchased[i];
+    }
+    return sum;
   }
 };
+firstNpike.cookiesPerHour();
+firstNpike.totalCookies();
 
+// 2nd Store SeaTac Airport
 var SeaTacAirport = {
   minCustPerHour: 3,
   maxCustPerHour: 24,
@@ -38,9 +49,19 @@ var SeaTacAirport = {
       console.log('Number of cookies purchased per hour is ' + this.cookiesPurchased[i]);
       return this.cookiesPurchased;
     }
+  },
+  totalCookies: function () {
+    var sum = 0;
+    for (var i = 0; i < hours.length ; i++) {
+      sum += this.cookiesPurchased[i];
+    }
+    return sum;
   }
 };
+SeaTacAirport.cookiesPerHour();
+SeaTacAirport.totalCookies();
 
+// 3rd Store SeattleCenter
 var SeattleCenter = {
   minCustPerHour: 11,
   maxCustPerHour: 38,
@@ -57,9 +78,19 @@ var SeattleCenter = {
       console.log('Number of cookies purchased per hour is ' + this.cookiesPurchased[i]);
       return this.cookiesPurchased;
     }
+  },
+  totalCookies: function () {
+    var sum = 0;
+    for (var i = 0; i < hours.length ; i++) {
+      sum += this.cookiesPurchased[i];
+    }
+    return sum;
   }
 };
+SeattleCenter.cookiesPerHour();
+SeattleCenter.totalCookies();
 
+// 4th Store CapitolHill
 var CapitolHill = {
   minCustPerHour: 20,
   maxCustPerHour: 38,
@@ -76,9 +107,19 @@ var CapitolHill = {
       console.log('Number of cookies purchased per hour is ' + this.cookiesPurchased[i]);
       return this.cookiesPurchased;
     }
+  },
+  totalCookies: function () {
+    var sum = 0;
+    for (var i = 0; i < hours.length ; i++) {
+      sum += this.cookiesPurchased[i];
+    }
+    return sum;
   }
 };
+CapitolHill.cookiesPerHour();
+CapitolHill.totalCookies();
 
+//5th Store Alki
 var Alki = {
   minCustPerHour: 2,
   maxCustPerHour: 16,
@@ -95,5 +136,14 @@ var Alki = {
       console.log('Number of cookies purchased per hour is ' + this.cookiesPurchased[i]);
       return this.cookiesPurchased;
     }
+  },
+  totalCookies: function () {
+    var sum = 0;
+    for (var i = 0; i < hours.length ; i++) {
+      sum += this.cookiesPurchased[i];
+    }
+    return sum;
   }
 };
+Alki.cookiesPerHour();
+Alki.totalCookies();
