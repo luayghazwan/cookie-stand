e64df33fc39275f58817a4d22d26e7e124fc89af
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-01-11 12:46:26 -0800

    Done with this Demo

--- app.js
+++ app.js
@@ -1,68 +1,25 @@
 'use strict';
 
-var hours = ['','6:00am','7:00am','8:00am','9:00am','10:00am', '11:00am', '12:00pm', '1:00pm','2:00pm:', '3:00pm:', '4:00pm:', '5:00pm:', '6:00pm:', '7:00pm:','8:00pm:', 'Daily Location Total'];
+var formEl = document.getElementById('first-form');
 
-var storesNames = ['','First & Pike','SeaTac Airport','Seattle Center','Capitol Hill','Alki'];
+formEl.addEventListener('submit', function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  console.log(event.target.username.value);
+  console.log(event.target.password.value);
+  //event.target is attached to formEl that has the parentheses ()
+  // value is used to get only the string inside the double quotation from the HTML
+  console.log(event.target);
 
-var cookiesPurchased = [];
+  var username = event.target.username.value;
+  renderUsername(username);
+}, false);
 
-function Store (minCustPerHour,maxCustPerHour,avgCookieSale) {
-  this.minCustPerHour = minCustPerHour;
-  this.maxCustPerHour = maxCustPerHour;
-  this.avgCookieSale = avgCookieSale;
+function renderUsername(username) { //passing username to the function
+  var sectionEl = document.getElementById('form-section');
+
+  var userNameEl = document.createElement('p');
+  userNameEl.setAttribute('class','entered-username');
+  userNameEl.textContent = username;
+  sectionEl.appendChild(userNameEl);
 }
-
-Store.prototype.randomNumber = function () {
-  var calculation = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
-  return calculation;
-};
-
-Store.prototype.cookiesPerHour = function () {
-  for (var i = 0; i < hours.length ; i++) {
-    var randomCustomers = this.randomNumber();
-    console.log(randomCustomers);
-    var amountCookies = Math.floor(this.avgCookieSale * randomCustomers);
-    console.log(amountCookies);
-    cookiesPurchased.push(amountCookies);}
-  return cookiesPurchased;
-};
-
-Store.prototype.totalCookiesPerDay = function () {
-  var sum = 0;
-  for (var i = 0; i < hours.length ; i++) {
-    sum += cookiesPurchased[i];
-  }
-  return sum;
-};
-
-// Stores.prototype.docTable = function() {
-//   var cookiesArray = this.cookiesPerHour();
-//   var totalSum = this.totalCookiesPerDay();
-//
-//   var tableEl = document.getElementById('cookie-table');
-//   var trEl = document.createElement('th');
-//   for (var i = 0; i < hours.length; i++){
-//     var rowData = hours[i];
-//     var thEl = document.createElement('th');
-//     thEl.textContent = rowDlata;
-//     trEl.appendChild(thEl);
-//   };
-//   tableEl.appendChild(trEl);
-//   for (var index = 0; index < stores.length; index++){
-//     trEl = document.createElement('tr');
-//     for (var j = 0; j < this.cookiesArray.length; j++){
-//       var tdEl = document.createElement('td');
-//       tdEl.textContent = this.cookiesArray[j];
-//       trEl.appendChild(tdEl);
-//     }
-//     tableEl.appendChild(trEl);
-//   }
-// };
-
-var firstAndPike = new Store (23,65,6.3);
-var SeaTacAirport = new Store (3,24,1.2);
-var SeattleCenter = new Store (11,38,3.7);
-var CapitolHill = new Store (20,38,2.3);
-var Alki = new Store (2,16,4.6);
-
-Stores.firstAndPike.cookiesPerHour();
